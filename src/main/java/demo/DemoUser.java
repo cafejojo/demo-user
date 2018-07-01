401f4ce079fe437aecaddc26777a2394ce48a677
Joël Abrahams <JSAbrahams@users.noreply.github.com>
2018-07-01 23:04:55 +0200

    Update DemoUser.java

--- src/main/java/demo/DemoUser.java
+++ src/main/java/demo/DemoUser.java
@@ -1,24 +1,20 @@
 package demo;
 
-public class DemoUser {
-	public void usageOne() {
-		String s = new DemoLibrary().neverEmptyString();
-		DemoLibrary.checkStringNotEmpty(s);
-	}
-	public void usageTwo() {
-		char c = new DemoLibrary().alwaysBChar();
-		DemoLibrary.checkCharIsB(c);
-	}
-	public void usageThree() {
-		int i = new DemoLibrary().alwaysPositiveInt();
-		DemoLibrary.checkIntIsPositive(i);
-	}
-	public void usageFour() {
-		double d = new DemoLibrary().alwaysPositiveDouble();
-		DemoLibrary.checkDoubleIsPositive(d);
-	}
-	public void usageFive() {
-		boolean b = new DemoLibrary().alwaysTrueBoolean();
-		DemoLibrary.checkBooleanIsTrue(b);
-	}
+import com.google.common.base.Charsets;
+import org.cafejojo.schaapi.test.library.LibraryClass;
+
+import java.nio.charset.Charset;
+
+public class UserClass {
+    public int userFoo() {
+    	LibraryClass l = new LibraryClass();
+        int x = l.foo();
+        double y = l.bar(x);
+
+        if (y > 5) {
+        	return 1;
+        } else {
+        	return 0;
+        }
+    }
 }
